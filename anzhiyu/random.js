8ab0eef6ea7700458b1d4967fb0d08a766539828
Y4n9Ch <1297893113@qq.com>
2023-10-09 09:35:12 +0800

    Site updated: 2023-10-09 09:35:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/09/29/Linux常用命令(1)/","2023/09/26/基础CTF-1/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/09/29/Linux常用命令(1)/","2023/09/26/基础CTF-1/","2023/10/09/Linux常用命令(2)/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
